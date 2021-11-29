9f4f62af484ff4eaf588b839c87dbc5e955fe714
Tyler Long <tyler4long@gmail.com>
2021-11-29 15:35:44 -0800

    html-webpack-plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,17 @@
 /* eslint-disable node/no-unpublished-import */
 import {Configuration} from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: Configuration = {
   mode: 'development',
   entry: {
     index: './src/index.ts',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Babylon.js WebXR Demo',
+    }),
+  ],
 };
 
 export default config;
