c809449fba0afde9a1ec9f2e777b155bca82dd3a
Tyler Long <tyler4long@gmail.com>
2021-11-29 16:19:47 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log('Hello world');
+import * as BABYLON from 'babylonjs';
+
+import './index.css';
+
+const canvas = document.createElement('canvas') as HTMLCanvasElement;
+canvas.id = 'renderCanvas';
+document.body.appendChild(canvas);
+
+(async () => {
+  const engine = new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+    disableWebGL2Support: false,
+  });
+  window.addEventListener('resize', () => {
+    engine.resize();
+  });
+
+  const scene = new BABYLON.Scene(engine);
+  const camera = new BABYLON.FreeCamera(
+    'camera1',
+    new BABYLON.Vector3(0, 5, -10),
+    scene
+  );
+  camera.setTarget(BABYLON.Vector3.Zero());
+  camera.attachControl(canvas, true);
+  const light = new BABYLON.HemisphericLight(
+    'light1',
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  );
+  light.intensity = 0.7;
+  const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+  sphere.position.y = 1;
+
+  const env = scene.createDefaultEnvironment()!;
+  await scene.createDefaultXRExperienceAsync({
+    floorMeshes: [env.ground!],
+  });
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+})();
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,30 @@
 /* eslint-disable node/no-unpublished-import */
 import {Configuration} from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
 
 const config: Configuration = {
   mode: 'development',
   entry: {
     index: './src/index.ts',
+  },
+  output: {
+    path: path.join(__dirname, 'docs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
